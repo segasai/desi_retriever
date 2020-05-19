b161896faec9ed70535a191e6c712c1ddc353932
Sergey Koposov <skoposov@cmu.edu>
2020-05-19 18:46:07 -0400

    put files in the repo

---
+++ py/desi_retriever/andes/fetcher.py
@@ -0,0 +1,162 @@
+import astropy.table as atpy
+import httpio
+import numpy as np
+import astropy.io.fits as pyfits
+import urllib3
+import os
+urllib3.disable_warnings()
+
+
+class si:
+    DESI_USER = None
+    DESI_PASSWD = None
+
+
+def get_desi_login_password():
+    if si.DESI_USER is None:
+        config = os.environ['HOME'] + '/.desi_http_user'
+        user, pwd = open(config).read().rstrip().split(':')
+        si.DESI_USER, si.DESI_PASSWD = user, pwd
+    return si.DESI_USER, si.DESI_PASSWD
+
+
+def get_specs(tileid=None,
+              night=None,
+              fiber=None,
+              targetid=None,
+              expid=None,
+              coadd=False):
+    """
+    Get DESI spectra 
+    
+    Parameters
+    ----------
+    tileid: int
+    night: int
+    fiber: int
+    targetid: int
+    expid: int (optional)
+    coadd: bool 
+         If true read coadded spectra
+
+    Returns
+    -------
+    ret: list(dict)
+        The list of dictionaries where each dictionary
+        has keywords b_wavelength, r_wavelength, z_wavelength
+        b_flux etc
+
+    """
+    if coadd:
+        prefix = 'coadd'
+    else:
+        prefix = 'spectra'
+    spectrograph = fiber // 500
+    url = f'https://data.desi.lbl.gov/desi/spectro/redux/andes/tiles/{tileid}/{night}/{prefix}-{spectrograph}-{tileid}-{night}.fits'
+    user, pwd = get_desi_login_password()
+    kw = dict(auth=(user, pwd), verify=False)
+    
+    with httpio.open(url, **kw) as fp:
+        hdus = pyfits.open(fp)
+        ftab = atpy.Table(hdus['FIBERMAP'].data)
+        
+        if expid is not None:
+            xind = ftab['EXPID'] == expid
+        else:
+            xind = np.ones(len(ftab))
+        xids = np.nonzero((ftab['TARGETID'] == targetid)&xind)[0]
+        if len(xids) == 0:
+            print('no spectra')
+            return
+        bwave = hdus['B_WAVELENGTH'].data
+        rwave = hdus['R_WAVELENGTH'].data
+        zwave = hdus['Z_WAVELENGTH'].data
+
+        rets = []
+        bdata = hdus['B_FLUX'].data
+        rdata = hdus['R_FLUX'].data
+        zdata = hdus['Z_FLUX'].data
+        rets = []
+        for xid in xids:
+            bdat = bdata[xid, :]
+            rdat = rdata[xid, :]
+            zdat = zdata[xid, :]
+
+            ret = dict(b_wavelength=bwave,
+                       r_wavelength=rwave,
+                       z_wavelength=zwave,
+                       b_flux=bdat,
+                       r_flux=rdat,
+                       z_flux=zdat)
+            rets.append(ret)
+        return rets
+
+
+def get_rvspec_models(tileid=None,
+                      night=None,
+                      fiber=None,
+                      targetid=None,
+                      expid=None,
+                      coadd=False,
+                      run='200507'):
+    """
+    Get RVSpecfit models
+    
+    Parameters
+    ----------
+    tileid: int
+    night: int
+    fiber: int
+    targetid: int
+    expid: int (optional)
+    coadd: bool 
+         If true read coadded spectra
+    run: string
+         The string identifying a software run
+
+    Returns
+    -------
+    ret: list(dict)
+        The list of dictionaries where each dictionary
+        has keywords b_wavelength, r_wavelength, z_wavelength
+        b_model etc
+    """
+
+    if coadd:
+        prefix = 'rvmod_spectra'
+    else:
+        prefix = 'rvmod_coadd'
+    spectrograph = fiber // 500
+    url = f'https://data.desi.lbl.gov/desi/science/mws/redux/andes/{run}/rv_output/{tileid}/{night}/{prefix}-{spectrograph}-{tileid}-{night}.fits'
+    user, pwd = get_desi_login_password()
+    kw = dict(auth=(user, pwd), verify=False)
+    with httpio.open(url, **kw) as fp:
+        hdus = pyfits.open(fp)
+        ftab = atpy.Table(hdus['FIBERMAP'].data)
+        xids = np.nonzero(ftab['TARGETID'] == targetid)[0]
+        if len(xids) == 0:
+            print('no spectra')
+            return
+        bwave = hdus['B_WAVELENGTH'].data
+        rwave = hdus['R_WAVELENGTH'].data
+        zwave = hdus['Z_WAVELENGTH'].data
+
+        rets = []
+        bdata = hdus['B_MODEL'].data
+        rdata = hdus['R_MODEL'].data
+        zdata = hdus['Z_MODEL'].data
+        rets = []
+        for xid in xids:
+            bdat = bdata[xid, :]
+            rdat = rdata[xid, :]
+            zdat = zdata[xid, :]
+
+            ret = dict(b_wavelength=bwave,
+                       r_wavelength=rwave,
+                       z_wavelength=zwave,
+                       b_model=bdat,
+                       r_model=rdat,
+                       z_model=zdat)
+            rets.append(ret)
+        return rets
+
---
+++ setup.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import os
+from setuptools import setup
+import glob
+import subprocess
+
+def get_revision():
+    """
+    Get the git revision of the code
+
+    Returns:
+    --------
+    revision : string
+        The string with the git revision
+    """
+    try:
+        tmpout = subprocess.Popen(
+            'cd ' + os.path.dirname(__file__) + ' ; git log -n 1 --pretty=format:%H -- setup.py',
+            shell=True, bufsize=80, stdout=subprocess.PIPE).stdout
+        revision = tmpout.read().decode()[:6]
+        return revision
+    except:
+        return ''
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+
+#exec(open('py/sqlutilpy/version.py','r').read())
+#VERSION  =__version__
+VERSION='0.0.1'
+VERSION1 = get_revision() 
+if VERSION1 != '':
+    VERSION = VERSION+'-git-'+VERSION1
+#VERSION = VERSIONPIP+'dev'+get_revision()
+
+setup(
+    name = "desi_retriever",
+    version = VERSION,
+    author = "Sergey Koposov",
+    author_email = "skoposov@cmu.edu",
+    description = (""),
+    license = "BSD",
+    keywords = "numpy postgresql query sql sqlite array",
+    url = "https://github.com/segasai/desi_retriever",
+    packages=['desi_retriever','desi_retriever/andes'],
+    #scripts = [fname for fname in glob.glob(os.path.join('bin', '*'))],
+    package_dir={'':'py'},
+#    package_data={'sqlutilpy':['tests']},
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    install_requires=open('requirements.txt').readlines(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
